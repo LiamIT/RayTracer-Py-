4181284a5cad7e67615fcd3ad3bd839016a30605
LiamIT <liamitinsley@gmail.com>
2022-04-05 20:44:21 -0500

    Initial Commit

---
+++ Ray.py
@@ -0,0 +1,7 @@
+from Vector3D import Vector3D
+
+
+class Ray:
+    def __init__(self, origin=Vector3D(0, 0, 0), direction=Vector3D(1, 0, 0)):
+        self.origin = origin
+        self.direction = direction
